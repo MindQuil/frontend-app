5459007d1f3dd363adf45b26db9a55e1ee5ec3bf
Nimah <busayonimota1603@gmail.com>
2023-03-12 22:01:17 +0100

    [Update]: App.jsx file

--- mindquil/src/App.jsx
+++ mindquil/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import styles from './style';
+import { Navbar, Hero, UserStory, Services } from './components';
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
-    <h1 className='text-white'></h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Services />
+        <UserStory />
+      </div>
+    </div>
   </div>
 );
 
