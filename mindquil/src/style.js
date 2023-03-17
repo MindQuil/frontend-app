d55ba99178ffea893eb454309c9e965fdcd39a86
Nimah <busayonimota1603@gmail.com>
2023-03-17 17:08:03 +0100

    [Update]: Js style

--- mindquil/src/style.js
+++ mindquil/src/style.js
@@ -4,12 +4,12 @@ const styles = {
   heading2:
     'font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full',
   paragraph:
-    'font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]',
+    'font-poppins font-normal text-offwhite text-[18px] leading-[20.8px] w-5%',
 
   flexCenter: 'flex justify-center items-center',
   flexStart: 'flex justify-center items-start',
 
-  paddingX: 'sm:px-16 px-6',
+  paddingX: 'sm:px-20 px-6',
   paddingY: 'sm:py-16 py-6',
   padding: 'sm:px-16 px-6 sm:py-12 py-4',
 
