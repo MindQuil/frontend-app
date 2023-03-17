3ab4f77e0997f4bfd029ea68e5ea43c6dd2ea545
Nimah <busayonimota1603@gmail.com>
2023-03-17 17:03:46 +0100

    [Update]: Assets

--- mindquil/src/assets/index.js
+++ mindquil/src/assets/index.js
@@ -1,5 +1,6 @@
 import logoX from './logoX.png';
 import menuClose from './menuClose.svg';
 import menuOpen from './menuOpen.svg';
+import mental from './mental.png';
 
-export { logoX, menuClose, menuOpen };
+export { logoX, menuClose, menuOpen, mental };
