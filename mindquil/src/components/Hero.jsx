e1f0bbe6a9d4782e625cff8810783f164a53a0ef
Nimah <busayonimota1603@gmail.com>
2023-03-18 22:33:58 +0100

    [Update]: Add component id

--- mindquil/src/components/Hero.jsx
+++ mindquil/src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => (
       />
     </div>
     <div className='sm:flex hidden absolute left-[50%] translate-x-[-50%] bottom-5 '>
-      <a href='userStory'>
+      <a href='#userStory'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
