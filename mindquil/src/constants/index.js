46a58ee557a1ef0b981b193a8ff37dafb71aad65
Nimah <busayonimota1603@gmail.com>
2023-03-18 22:25:07 +0100

    [Feats]: Add users' story data

--- mindquil/src/constants/index.js
+++ mindquil/src/constants/index.js
@@ -1,3 +1,5 @@
+import { people01, people02, people03 } from '../assets';
+
 export const navButtons = [
   {
     id: 'home',
@@ -14,5 +16,32 @@ export const navButtons = [
   {
     id: 'contact',
     title: 'Contact',
+  },
+];
+
+export const feedback = [
+  {
+    id: 'story-1',
+    content:
+      'I would get so frustrated because it seemed impossible to sleep, as if insomnia and depression go hand in hand. I knew I needed help but asking for it just made me feel like a burden. Thanks to the docs on MindQuil. Now I feel more alive than ever',
+    name: 'Abeeb Kayode',
+    title: 'MindQuil User',
+    img: people01,
+  },
+  {
+    id: 'story-2',
+    content:
+      'There is comfort in knowing that you’re not the only one experiencing these awful depression symptoms and effects of depression. MindQuil gave me that comfort',
+    name: 'Tamada Tom',
+    title: 'MindQuil User',
+    img: people02,
+  },
+  {
+    id: 'story-3',
+    content:
+      'MindQuil is a testament that mental health problems are highly treatable. The platform provides all the support needed to work through issues with my clients',
+    name: 'Christiana Bells',
+    title: 'Therapist',
+    img: people03,
   },
 ];
