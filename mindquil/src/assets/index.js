ef62953079ba4677959aa638c43440c2c94792af
Nimah <busayonimota1603@gmail.com>
2023-03-18 22:24:20 +0100

    [Feats]: Add userrs' story assets

--- mindquil/src/assets/index.js
+++ mindquil/src/assets/index.js
@@ -2,5 +2,18 @@ import logoX from './logoX.png';
 import menuClose from './menuClose.svg';
 import menuOpen from './menuOpen.svg';
 import mental from './mental.png';
+import people01 from './people01.jpg';
+import people02 from './people02.jpg';
+import people03 from './people03.jpg';
+import quotes from './quote.svg';
 
-export { logoX, menuClose, menuOpen, mental };
+export {
+  logoX,
+  menuClose,
+  menuOpen,
+  mental,
+  people01,
+  people02,
+  people03,
+  quotes,
+};
