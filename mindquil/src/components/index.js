88d9927bbc8fc370ce77893982359ef0f997e5d6
Nimah <busayonimota1603@gmail.com>
2023-03-19 03:30:22 +0100

    [Update]: Add avatar

--- mindquil/src/components/index.js
+++ mindquil/src/components/index.js
@@ -4,7 +4,7 @@ import Services from './Services';
 import UserStory from './UserStory';
 import Button from './Button';
 import StoryCard from './StoryCard';
-import Blank from './UserImg';
+import Blank from './Avatar';
 import Logo from './Logo';
 
 export { Navbar, Hero, Services, UserStory, Button, StoryCard, Blank, Logo };
