bdeb9ef0d9a133cd8eed8280c9ee99ad51469a43
Nimah <busayonimota1603@gmail.com>
2023-03-21 08:27:23 +0100

    [Update]: About component

--- mindquil/src/constants/index.js
+++ mindquil/src/constants/index.js
@@ -23,15 +23,14 @@ export const feedback = [
   {
     id: 'story-1',
     content:
-      'I would get so frustrated because it seemed impossible to sleep, as if insomnia and depression go hand in hand. I knew I needed help but asking for it just made me feel like a burden. Thanks to the docs on MindQuil. Now I feel more alive than ever',
+      'I would get so frustrated because it seemed impossible to sleep, as if insomnia and depression go hand in hand. I knew I needed help but asking for it just made me feel like a burden. Thanks to the docs on MindQuil. Now I feel more alive than ever.',
     name: 'Abeeb Kayode',
     title: 'MindQuil User',
     img: people01,
   },
   {
     id: 'story-2',
-    content:
-      'There is comfort in knowing that you’re not the only one experiencing these awful depression symptoms and effects of depression. MindQuil gave me that comfort',
+    content: `There is comfort in knowing that you’re not the only one experiencing these awful depression symptoms and effects of depression. MindQuil gave me that comfort.`,
     name: 'Tamada Tom',
     title: 'MindQuil User',
     img: people02,
@@ -39,9 +38,21 @@ export const feedback = [
   {
     id: 'story-3',
     content:
-      'MindQuil is a testament that mental health problems are highly treatable. The platform provides all the support needed to work through issues with my clients',
+      'MindQuil is a testament that mental health problems are highly treatable. The platform provides all the support needed to work through issues with my clients.',
     name: 'Christiana Bells',
     title: 'Therapist',
     img: people03,
+  },
+];
+
+export const aboutUs = [
+  {
+    id: 'about-01',
+    content:
+      'is a web application designed to improve mental health outcomes and help you prioritize your emotional wellbeing and achieve better mental health. It is a user-friendly, accessible platform that provides individuals with various resources to support their mental health journey.',
+  },
+  {
+    id: 'about-02',
+    content: `provides access to mental health professionals who can offer virtual therapy sessions to users. Users can also connect with other individuals who are going through similar challenges through the web app's support group.`,
   },
 ];
