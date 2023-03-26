09f43bee13c81f927b36baeab4480d0f01ea7726
Nimah <busayonimota1603@gmail.com>
2023-03-27 00:47:45 +0100

    [Update]: Login features

--- mindquil/src/routes/index.js
+++ mindquil/src/routes/index.js
@@ -1,4 +1,7 @@
 import App from './App';
 import About from './About';
+import Register from './Register';
+import Error from './Error';
+import Login from './LogIn';
 
-export { App, About };
+export { App, About, Register, Error, Login };
